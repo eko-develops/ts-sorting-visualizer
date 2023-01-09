463370619d084f13adc8ae3633731b6054923a95
eko-develops <ekodevelops@gmail.com>
2023-01-08 23:52:17 -0800

    update

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -62,17 +62,28 @@ export async function insertionSort(array: HTMLDivElement[]) {
 export async function selectionSort(array: HTMLDivElement[]) {
 	for (let i = 0; i < array.length; i++) {
 		let min = i;
+
 		for (let j = i + 1; j < array.length; j++) {
-			if (array[min] > array[j]) {
+			selectDiv(array[min], 'min');
+			selectDiv(array[j], 1);
+			await delay(1000);
+			if (Number(array[min].dataset.value) > Number(array[j].dataset.value)) {
+				deselectDiv(array[min]);
 				min = j;
 			}
+
+			deselectDiv(array[j]);
 		}
+
 		let temp = array[i];
+		deselectDiv(temp);
 		array[i] = array[min];
 		array[min] = temp;
-	}
+		deselectDiv(array[i]);
 
-	return array;
+		await delay(1000);
+		displayBars(array);
+	}
 }
 
 // function simpleSort(array) {
