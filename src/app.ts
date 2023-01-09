f60247a75d8bd2f8d5522c8dd1e111023429e6f9
eko-develops <ekodevelops@gmail.com>
2023-01-08 22:40:00 -0800

    remove comments

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,12 @@ function main() {
 	let globalArray: HTMLDivElement[] = createBars(MAX_ARRAY_SIZE, 1, 100);
 	displayBars(globalArray);
 
-	// const randomizeButton = document.querySelector(
-	// 	'#randomize'
-	// ) as HTMLButtonElement;
-
 	document.querySelector('#randomize')!.addEventListener('click', () => {
 		globalArray = createBars(MAX_ARRAY_SIZE, 1, 100);
 		displayBars(globalArray);
 	});
 
-	// start button click
 	document
 		.querySelector('#search')!
 		.addEventListener('click', () => startSearch(globalArray));
-
-	// testSortingAlgs();
 }
